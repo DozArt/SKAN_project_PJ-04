853d1ff1e89153d5156b8787ab84e1abe7855ed0
Your Name <you@example.com>
2023-12-23 01:03:39 +0300

    end Slider and WhyAs

--- SCAN_front/src/WhyAs/WhyAs.jsx
+++ SCAN_front/src/WhyAs/WhyAs.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import s from './WhyAs.module.css'
-import "slick-carousel/slick/slick.css";
+import "./slick.css";
 import "./slick-theme.css";
-import Slider, {PrevArrow2} from "react-slick";
+import Slider from "react-slick";
+import mascot_ok from "./images/mascot_ok.svg"
+import img_clock from "./images/342eec6c4e64fc1f6123156254eb7dd3.svg"
+import img_search from "./images/0fb8f25cf4a72c55c77ade958e46a450.svg"
+import img_security from "./images/png-transparent-computer-icons.svg"
+
+
+const WhyAs_item = ({id, icon, text}) => {
+    return (
+        <div key={id} className={s.item}>
+            <img src={icon} />
+            <p>{text}</p>
+        </div>
+    );
+}
+
+// на кодревью нет времени
+// map крашит Slider
+// а перенос строки - по другому не нашел решения
 
 
 const WhyAs = () => {
@@ -19,25 +37,38 @@ const WhyAs = () => {
         <div className={s.unit}>
             <h2 className={s.title}>Почему именно мы</h2>
             <Slider {...settings}>
-                <div>
-                    <h3>11111</h3>
-                </div>
-                <div>
-                    <h3>22222</h3>
-                </div>
-                <div>
-                    <h3>33333</h3>
-                </div>
-                <div>
-                    <h3>44444</h3>
-                </div>
-                <div>
-                    <h3>55555</h3>
-                </div>
-                <div>
-                    <h3>66666</h3>
-                </div>
+
+                <WhyAs_item id="1"
+                            icon={img_clock} 
+                            text="Высокая и оперативная
+скорость обработки заявки" />
+
+                <WhyAs_item id="2"
+                            icon={img_search} 
+                            text='Огромная комплексная база
+данных, обеспечивающая 
+объективный ответ на запрос' />
+                            
+                <WhyAs_item id="3"
+                            icon={img_security} 
+                            text="Защита конфеденциальных сведений, не подлежащих разглашению по федеральному законодательству" />
+
+                <WhyAs_item id="4"
+                            icon={img_clock} 
+                            text="Высокая и оперативная скорость обработки заявки" />
+
+                <WhyAs_item id="2"
+                            icon={img_search} 
+                            text="Огромная комплексная база данных, обеспечивающая объективный ответ на запрос" />
+
+                <WhyAs_item id="3"
+                            icon={img_security} 
+                            text="Защита конфеденциальных сведений,
+не подлежащих разглашению по
+федеральному законодательству" />
+
             </Slider>
+            <img className={s.mascot_ok}src={mascot_ok} />
         </div>
     );
 };
