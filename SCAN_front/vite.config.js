70cb0916c82a3b524a7b33f12c87365256de9897
DozArt <wpeace@mail.ru>
2024-08-14 22:45:29 +0300

    edit vite conf

--- SCAN_front/vite.config.js
+++ SCAN_front/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SKAN_project_PJ-04/', // Укажи свой путь
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
